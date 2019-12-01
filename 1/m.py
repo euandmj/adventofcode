a868d8308c56af02926f602fefe7f99a8171df48
Euan Jones <euandmj97@gmail.com>
2019-12-01 21:47:58 +0000

    day1

---
+++ 1/m.py
@@ -0,0 +1,20 @@
+from numpy import floor, sum
+
+def calc_fuel(mass):
+    return floor(mass / 3) - 2
+
+with open("modules.txt") as f:
+    modules = [int(x) for x in f.readlines()]
+
+all_fuels = [calc_fuel(x) for x in modules]
+
+total = 0 # sum(all_fuels)
+
+for m in modules:
+    t = calc_fuel(m)
+    total += t
+    while(calc_fuel(t) > 0):
+           t = calc_fuel(t)
+           total += t    
+
+print(total)
