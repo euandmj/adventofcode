bf691424d06dd678cedd38cd77a29daa09585e76
Euan Jones <euandmj97@gmail.com>
2019-12-01 23:12:06 +0000

    refactored

--- 1/m.py
+++ 1/m.py
@@ -3,18 +3,12 @@ from numpy import floor, sum
 def calc_fuel(mass):
     return floor(mass / 3) - 2
 
+def total_fuel(fuel):
+    fl = calc_fuel(fuel)
+    return fl + total_fuel(fl) if fl > 0 else 0
+
 with open("modules.txt") as f:
     modules = [int(x) for x in f.readlines()]
-    
-all_fuels = [calc_fuel(x) for x in modules]
-# fuelsum = sum(all_fuels)
 
-total = 0
-for m in modules:
-    t = calc_fuel(m)
-    total += t
-    while(calc_fuel(t) > 0):
-           t = calc_fuel(t)
-           total += t    
-
-print(total)
+star1 = sum(calc_fuel(x) for x in modules)
+star2 = sum(total_fuel(x) for x in modules)
